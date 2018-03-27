cec56830964b5b1881bb959e8c5f3b4cf4302f82
Fernando Minoru Baba <fernando.fminoru@gmail.com>
2018-03-27 21:34:32 +1100

    Add some comments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,10 @@ import { AppComponent } from './app.component';
 import { DynamicContainerModule } from './dynamic-container/dynamic-container.module';
 import * as Test from './test/test.module';
 
+
+// This is exactly the same thing I've been doing on my app.
+// It works with version 2.0.3 but it breaks with 2.1.0
+// On this sample project, it onlu works if I remove the input from the DynamicContainerComponent.
 @NgModule({
   declarations: [
     AppComponent
